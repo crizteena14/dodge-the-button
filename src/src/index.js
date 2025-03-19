0904baf46a2b7692f72be138090c84acf697a1e2
crizteena14 <christeenasimon14@gmail.com>
2025-03-19 20:03:28 +0530

    Create index.js

---
+++ src/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import DodgeButton from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DodgeButton />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
